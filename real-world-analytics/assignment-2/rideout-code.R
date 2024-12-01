9bca02545ac463afcdf1df75e5303b93ef02fee3
mick <mickrideout@gmail.com>
2024-12-02 09:19:53 +1100

    real world data analytics assignment 2 checkpoint

--- real-world-analytics/assignment-2/rideout-code.R
+++ real-world-analytics/assignment-2/rideout-code.R
@@ -2,6 +2,7 @@ library(pastecs) # For descriptive statistics (for stat.desc)
 library(car) # For qqPlot
 library(ggplot2) # ggplot
 library(knitr) # matrix printing
+library(corrplot) # correlation matrix plotting
 
 # Set seed as student id
 set.seed(225065259)
@@ -53,11 +54,11 @@ office_room_humidity = "Office Room Humidity (%) (X4)"
 pressure = "Pressure (mm of mercury) (X5)"
 target = "Appliances Energy Consumption (Wh) (Y)"
 
-# Create column indicies
+# Create column indices
 lr_temp_col_name = "X1"
 lr_humidity_col_name = "X2"
 or_temp_col_name = "X3"
-or_humidity_index = "X4"
+or_humidity_col_name = "X4"
 pressure_col_name = "X5"
 target_col_name = "Y"
 
@@ -78,11 +79,31 @@ scatter_plot <- function(x_data,
 }
 
 # Generate scatter plots for all X variables against Y
-scatter_plot(my.data[,lr_temp_col_name], my.data[,target_col_name], living_room_temperature, target, paste(living_room_temperature, " vs ", target))
-scatter_plot(my.data[,lr_humidity_col_name], my.data[,target_col_name], living_room_humidity, target, paste(living_room_humidity, " vs ", target))
-scatter_plot(my.data[,or_temp_col_name], my.data[,target_col_name], office_room_temperature, target, paste(office_room_temperature, " vs ", target))
-scatter_plot(my.data[,or_humidity_index], my.data[,target_col_name], office_room_humidity, target, paste(office_room_humidity, " vs ", target))
-scatter_plot(my.data[,pressure_col_name], my.data[,target_col_name], pressure, target, paste(pressure, " vs ", target))
+scatter_plot(my.data[,lr_temp_col_name],
+             my.data[,target_col_name],
+             living_room_temperature,
+             target, 
+             paste(living_room_temperature, " vs ", target))
+scatter_plot(my.data[,lr_humidity_col_name],
+             my.data[,target_col_name],
+             living_room_humidity,
+             target,
+             paste(living_room_humidity, " vs ", target))
+scatter_plot(my.data[,or_temp_col_name],
+             my.data[,target_col_name],
+             office_room_temperature,
+             target,
+             paste(office_room_temperature, " vs ", target))
+scatter_plot(my.data[,or_humidity_col_name],
+             my.data[,target_col_name],
+             office_room_humidity,
+             target,
+             paste(office_room_humidity, " vs ", target))
+scatter_plot(my.data[,pressure_col_name],
+             my.data[,target_col_name],
+             pressure,
+             target,
+             paste(pressure, " vs ", target))
 
 # A helper function to create a histogram for a variable
 histogram <- function(data,   # The data matrix
@@ -113,7 +134,7 @@ generate_statistical_checks <- function(data,           # The data matrix
     # QQ Plot
     print(paste("Col name: ", col_name))
     df <- as.data.frame(data)
-    plot <-ggplot(df, aes(sample = !!sym(col_name)))+stat_qq()+stat_qq_line(color = "red")+theme_minimal() +                # Apply a minimal theme
+    plot <-ggplot(df, aes(sample = !!sym(col_name)))+stat_qq()+stat_qq_line(color = "red")+theme_minimal()  
       labs(title = paste("Q-Q Plot of", data_process_level, label),
            x = "Theoretical Quantiles",
            y = "Sample Quantiles")
@@ -124,16 +145,17 @@ generate_statistical_checks <- function(data,           # The data matrix
     desc <- round(desc,2)
     print(paste("Descriptive statistics for: ", label))
     print("----------------------------")
-    kable(desc)
+    print(kable(desc))
+    print("----------------------------")
   }
 }
 
-# print pre transform raw graphs of variables
+# print pre transformed raw graphs of variables
 pre_transform_args <- matrix(
   c(lr_temp_col_name, living_room_temperature, " Raw ",
     lr_humidity_col_name, living_room_humidity, " Raw ",
     or_temp_col_name, office_room_temperature, " Raw ",
-    or_humidity_index, office_room_humidity, " Raw ",
+    or_humidity_col_name, office_room_humidity, " Raw ",
     pressure_col_name, pressure, " Raw ",
     target_col_name, target, " Raw "
     ),
@@ -142,5 +164,61 @@ pre_transform_args <- matrix(
 
 generate_statistical_checks(my.data, pre_transform_args)
 
+# Generate the correlation plot
+cor_matrix <- cor(my.data) 
+corrplot(cor_matrix, method = "circle", addCoef.col = 'black')
 
+
+################################################################################
+# T2 - Transform the data
+################################################################################
+
+# Drop the X4 (office room humidity) feature as it has the lowest correlations 
+# to target
+# TODO confirm other measures of selection
+my.data <- my.data[, colnames(my.data) != or_humidity_col_name]
+stopifnot(ncol(my.data) == 5) # assert that we now have 5 columns
+
+
+# Function to transform the matrix data
+# input: data - a data matrix with 5 columns (the last being the target)
+transform_data <- function(data) {
+  local_data_copy <- data # make a copy of the matrix
+  local_data_copy[, lr_temp_col_name] <- log(local_data_copy[, lr_temp_col_name])
+  local_data_copy[, lr_humidity_col_name] <- local_data_copy[, lr_humidity_col_name] ^ (1/2)
+  # No transformation on or_temp_col_name as the distribution is normal
+  local_data_copy[, pressure_col_name] <- local_data_copy[, pressure_col_name] ^ (1/2)
+  local_data_copy[, target_col_name] <- local_data_copy[, target_col_name] ^ 2
+  local_data_copy
+}
+
+# Transform the input matrix
+transformed.data <- transform_data(my.data)
+
+# graph transformed data
+# print pre transformed raw graphs of variables
+post_transform_args <- matrix(
+  c(lr_temp_col_name, living_room_temperature, " Transformed ",
+    lr_humidity_col_name, living_room_humidity, " Transformed ",
+    or_temp_col_name, office_room_temperature, " Transformed ",
+    pressure_col_name, pressure, " Transformed ",
+    target_col_name, target, " Transformed "
+  ),
+  ncol=3, byrow = TRUE
+)
+
+generate_statistical_checks(transformed.data, post_transform_args)
+
+# We need to store the training data mean and standard deviation for later 
+# scaling of inputs during inference
+training_data_means <- matrix(
+  c(
+    mean(transformed.data[, lr_temp_col_name]),
+    mean(transformed.data[, lr_humidity_col_name]),
+    mean(transformed.data[, or_temp_col_name]),
+    mean(transformed.data[, pressure_col_name]),
+    mean(transformed.data[, target_col_name]),
+    ncol=5, byrow = TRUE
+  )
+)
 
